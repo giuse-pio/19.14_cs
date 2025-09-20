f768af35b94134c41bd93e685509582e2e2feced
giupe <giupe@MSI>
2025-09-20 16:55:28 +0200

    Add project files.

---
+++ 19.14/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _19._14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numeri = new List<int>();
+            for (int i = 0; i < 30; i++)
+            {
+                numeri.Add(0);
+                i++;
+                numeri.Add(1);
+            }
+            foreach (var numero in numeri)
+            {
+                Console.WriteLine(numero);
+            }
+        }
+    }
+}
